120b385da4ac8d9b203173561dbabf2c4994624c
Mahmudul Hasan <mhcybroot@gmail.com>
2026-02-11 11:04:44 +0600

    implemented authorization

---
+++ src/guards/roles/roles.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from './roles.enum';
+import { ROLES_KEY } from './roles.decoretor';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector : Reflector){}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY,[
+        context.getHandler(),
+        context.getClass()
+      ]
+    );
+
+    if(!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest<{headers : Record<string,string>}>();
+
+    const userRole = request.headers['x-user-role'] as Role;
+    return requiredRoles.includes(userRole);
+  
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiHeader } from '@nestjs/swagger';
+import { Roles } from 'src/guards/roles/roles.decoretor';
+import { Role } from 'src/guards/roles/roles.enum';
+import { RolesGuard } from 'src/guards/roles/roles.guard';
+
 
 @Controller('user')
 export class UserController {
   @Get()
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
+  @ApiHeader({
+    name: 'x-user-role',
+    description: 'The role of the user making the request',
+    required: true,
+    enum: Role, // If Role is an enum, Swagger will create a dropdown!
+  })
   getUser() {
     return 'User data fetched successfully';
   }
